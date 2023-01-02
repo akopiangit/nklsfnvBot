87af68ee8ef302baa7f941f259167d002e829a1d
agakopian <agakopian@domclick.ru>
2023-01-02 19:40:50 +0300

    add database

---
+++ src/main/java/ru/nklsfnv/nklsfnvbot/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ru.nklsfnv.nklsfnvbot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.nklsfnv.nklsfnvbot.model.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
